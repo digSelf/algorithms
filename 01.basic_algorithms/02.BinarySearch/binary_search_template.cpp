745bb8fff8b9c8c7336a719e1634ac09e7c6a4a2
iforget <com.alvin.li@outlook.com>
2022-02-21 12:29:10 +0800

    更新二分查找模板注释代码

--- 01.basic_algorithms/02.BinarySearch/binary_search_template.cpp
+++ 01.basic_algorithms/02.BinarySearch/binary_search_template.cpp
@@ -12,6 +12,7 @@ void binarySearch(const vector<int>& arr, int start, int end, int k) {
     // 退出条件是：left >= right，left的值能够大于right吗？不能的，最多是等于
     // 为什么？因为left和right的更新是由mid来进行更新的，而mid in [left, right]中，不会超出该区间
     // 所以退出条件一定是left == right时退出
+    // 注意，不能写成`left <= right`的形式，因为可能会造成死循环
     while (left < right) {
         int mid = (left + right) >> 1; // 注意是向下取整
         
